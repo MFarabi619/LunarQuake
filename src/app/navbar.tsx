e9b7519005d8b2b5db16721158bc48a59a4a3578
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-07 11:52:58 -0400

    feat: Add hover effects to navbar elements

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="flex border border-dashed px-8 py-4 justify-between">
-      <section>LunarQuake</section>
+      <section className="transition hover:cursor-pointer hover:scale-125 duration-500 ease-in-out">LunarQuake</section>
       <section>
         <nav className="flex">
-          <Link href={"/"} className="px-4">
+          <Link href={"/"} className="px-4 transition hover:cursor-pointer hover:scale-125 duration-500 ease-in-out">
             Home
           </Link>
-          <Link href={"/"} className="px-4">
+          <Link href={"/"} className="px-4 transition hover:cursor-pointer hover:scale-125 duration-500 ease-in-out">
             About
           </Link>
         </nav>
