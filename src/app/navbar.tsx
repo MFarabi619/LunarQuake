4257597630577b2b35927825bfe8dcf14eea6df3
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-07 12:10:26 -0400

    feat: Add 'live' badge to Navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Link from "next/link";
+import {Badge, BadgeDelta } from "@tremor/react"
+import { StatusOnlineIcon } from "@heroicons/react/outline"
 
 export default function Navbar() {
   return (
@@ -12,6 +16,7 @@ export default function Navbar() {
           <Link href={"/"} className="px-4 transition hover:cursor-pointer hover:scale-125 duration-500 ease-in-out">
             About
           </Link>
+          <Badge size="lg" icon={StatusOnlineIcon} tooltip="Server is up with live data">live</Badge>
         </nav>
       </section>
     </header>
