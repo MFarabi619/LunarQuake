c94aa0bb8e82a68141d52917a7f3e89b8cac8689
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-07 22:30:01 -0400

    feat: Working marker on surface of moon

--- frontend/src/components/MoonCanvas.tsx
+++ frontend/src/components/MoonCanvas.tsx
@@ -1,38 +1,61 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
+const MOON_MODEL_RADIUS = 865.6653264873212; 
+
 const Moon = () => {
   const model = useGLTF("/moon.glb");
-  return <primitive object={model.scene} scale={0.5} />;
+  return <primitive object={model.scene} />;
+};
+
+const Marker: React.FC<{ }> = () => {
+  // Provided the latitude and longitude, convert to radians
+  const latitude = -3.00942 * (Math.PI / 180);
+  const longitude = -23.42458 * (Math.PI / 180);
+
+  const phi = Math.PI / 2 - latitude;
+  const theta = Math.PI + longitude;
+
+  // The marker radius is a percentage of the moon radius
+  const markerRadiusOffset = MOON_MODEL_RADIUS * 0.01;
+  const r = MOON_MODEL_RADIUS-365;
+
+  const x = -r * Math.sin(phi) * Math.sin(theta);
+  const y = r * Math.cos(phi);
+  const z = r * Math.sin(phi) * Math.cos(theta);
+
+  return (
+    <mesh position={[x, y, z]}>
+      <sphereGeometry args={[markerRadiusOffset, 16, 16]} />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  );
 };
 
 export default function MoonCanvas() {
+
   return (
-    <Canvas camera={{ position: [0, 0, 500] }}>
+    <Canvas camera={{ position: [0, 0, 1000] }}>
+      {/* Allow the user to control the camera with the mouse. */}
       <OrbitControls />
 
       {/* Ambient light affects all objects in the scene globally. */}
       <ambientLight intensity={3} />
 
       {/* Directional light acts like the sun, providing parallel light rays. */}
-      <directionalLight 
-        intensity={3}
-        position={[2, 2, 2]} 
-        castShadow={true}
-      />
+      <directionalLight intensity={3} position={[2, 2, 2]} />
 
-      {/* Point lights emit light in every direction from a single point. Adding this for additional brightness and highlights. */}
-      <pointLight 
-        intensity={0.5} 
-        position={[-5, 5, -5]}
-      />
+      {/* Point lights emit light in every direction from a single point. */}
+      <pointLight intensity={0.5} position={[-5, 5, -5]} />
 
-      {/* Adding a hemisphere light to softly illuminate the scene and give a more natural look. */}
-      <hemisphereLight 
-        intensity={0.5} 
-      />
+      {/* Hemisphere light to softly illuminate the scene and give a more natural look. */}
+      <hemisphereLight intensity={0.5} />
 
+      {/* The moon model. */}
       <Moon />
+
+      {/* The marker to indicate the central station. */}
+      <Marker/>
     </Canvas>
   );
 }
