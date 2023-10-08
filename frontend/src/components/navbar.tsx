f3d7d23ac100ab7aec42e70dc9cbf6b525bc88af
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-08 03:23:00 -0400

    feat: Make navbar glassmorphic and set max width

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,17 +1,18 @@
-"use client";
+"use client"
 
 import Link from "next/link";
 import { Button } from "@tremor/react";
-import { Badge, BadgeDelta } from "@tremor/react";
+import { Badge } from "@tremor/react";
 import { StatusOnlineIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
   return (
-    <header className="flex border border-dashed px-8 py-4 justify-between">
+    <header className="mt-2 mx-2 fixed w-full max-w-screen-2xl box-border z-10 flex px-8 py-4 justify-between bg-transparent bg-opacity-5 backdrop-blur-md rounded-xl border border-slate-300 border-opacity-20 shadow-lg">
+
       <section className="transition hover:cursor-pointer hover:scale-125 duration-500 ease-in-out">
         LunarQuake
       </section>
-      <section>
+      <section className="hidden md:block">
         <nav className="flex">
           <Link href={"/"}>
             <Button
